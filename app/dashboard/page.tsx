d7013c895f5c2cde27fe81bf7b699e33f42aae89
russellw999 <russellw999@gmail.com>
2023-11-05 18:21:43 +0700

    chapter 7 completed LatestInvoices component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,10 @@ import { Card } from '@/app/ui/dashboard/cards'
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue } from '@/app/lib/data';
+import { fetchRevenue , fetchLatestInvoices} from '@/app/lib/data';
 const Page = async () => {
   const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:2xl`}>Dashboard</h1>
@@ -20,7 +21,7 @@ const Page = async () => {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg-grid-cols-8">
          {<RevenueChart revenue={revenue}  /> }
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        {<LatestInvoices latestInvoices={latestInvoices} /> }
       </div>
    </main>
   );

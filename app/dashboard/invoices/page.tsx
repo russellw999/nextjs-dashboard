31b5fc55abc37d761edf1c27ecb7cba166f1af37
russellw999 <russellw999@gmail.com>
2023-11-11 00:31:53 +0700

    ch11 complete - pagination

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,8 +2,23 @@ import Pagination from '@/app/ui/invoices/pagination'
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table'
 import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import { Suspense } from 'react';
 import { lusitana } from '@/app/ui/fonts';
-const Invoices = () => {
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { fetchInvoicesPages } from '@/app/lib/data';
+const Invoices = async ({
+  searchParams,
+}: {
+   searchParams?: {
+     query?: string;
+     page?: string;
+   }
+}) => {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await fetchInvoicesPages(query);
+
   return (
     <div className='w-full'>
       <div className="flex w-full items-center justify-betwen">
@@ -13,11 +28,11 @@ const Invoices = () => {
         <Search placeholder="Search invoices..." />
         <CreateInvoice /> 
       </div>
-      {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
          <Table query={query} currentPage={currentPage} />
-      </Suspense> */}
+      </Suspense> 
       <div className="mt-5 flex w-full justify-center">
-        {/* <Pagination totalPages={totalPages} /> */}
+        <Pagination totalPages={totalPages} />
       </div>
   </div>
   );

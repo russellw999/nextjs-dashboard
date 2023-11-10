ed235dbe575d189ca3503b4a28f9dacbd05757cb
russellw999 <russellw999@gmail.com>
2023-11-11 00:35:29 +0700

    ch11 fully complete Pagination

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -14,7 +14,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
   // NOTE: comment in this code when you get to this point in the course
 
-  // const allPages = generatePagination(currentPage, totalPages);
+   const allPages = generatePagination(currentPage, totalPages);
 
   const createPageURL = ( pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
@@ -26,7 +26,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
     <>
       {/* NOTE: comment in this code when you get to this point in the course */}
 
-      {/* <div className="inline-flex">
+       <div className="inline-flex">
         <PaginationArrow
           direction="left"
           href={createPageURL(currentPage - 1)}
@@ -59,7 +59,7 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
           href={createPageURL(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div> 
     </>
   );
 }

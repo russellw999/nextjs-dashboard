92f925d1890fc3278eeba89e3263205e752f42c7
russellw999 <russellw999@gmail.com>
2023-11-11 23:35:41 +0700

    CH11 - 4 Validate Prep Data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,25 @@
 'use server'
 
+import { z } from 'zod';
+
+const InvoiceSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending','paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({id: true, date: true});
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+   // const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    }
-   // Test it
-   console.log(rawFormData)
-};
+     })  
+     const amountInCents = amount * 100; 
+     const date = new Date().toDateString().split('T')[0];
+}
+ 
